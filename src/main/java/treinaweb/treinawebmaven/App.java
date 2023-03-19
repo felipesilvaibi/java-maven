e58a3e99564f37c8105ca5353478f655104d5073
felipesilvaibi <felipe_ibirama@hotmail.com>
2023-03-19 17:10:04 -0300

    feat: add spring boot starter web and remove HTTPClient

--- src/main/java/treinaweb/treinawebmaven/App.java
+++ src/main/java/treinaweb/treinawebmaven/App.java
@@ -1,23 +1,11 @@
 package treinaweb.treinawebmaven;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
     public static void main( String[] args ) {
-        String url = "https://viacep.com.br/ws/" + args[0] + "/json/";
-
-        try {
-            CloseableHttpClient client = HttpClientBuilder.create().build();
-            CloseableHttpResponse response = client.execute(new HttpGet(url));
-            String json = EntityUtils.toString(response.getEntity());
-
-            System.out.println(json);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        SpringApplication.run(App.class, args);
     }
 }
