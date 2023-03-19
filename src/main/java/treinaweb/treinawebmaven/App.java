e59ad0be177dc7e706582508ac40b295de5f78b6
felipesilvaibi <felipe_ibirama@hotmail.com>
2023-03-19 15:13:12 -0300

    feat: add HttpClient request

--- src/main/java/treinaweb/treinawebmaven/App.java
+++ src/main/java/treinaweb/treinawebmaven/App.java
@@ -1,13 +1,23 @@
 package treinaweb.treinawebmaven;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+public class App {
+    public static void main( String[] args ) {
+        String url = "https://viacep.com.br/ws/" + args[0] + "/json/";
+
+        try {
+            CloseableHttpClient client = HttpClientBuilder.create().build();
+            CloseableHttpResponse response = client.execute(new HttpGet(url));
+            String json = EntityUtils.toString(response.getEntity());
+
+            System.out.println(json);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
